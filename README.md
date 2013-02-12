50fdf76addd299ff8b0b169e896d1b34091373da
Alex Latchford <alex.latchford@gmail.com>
2013-02-12 17:44:51 +0000

    Reset v1.0.3 changes, apparently `npm version build` doesn't quite do what I need it to do..

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ## Test Build
 
-Simple script to put in front of your test build server to change the package name prior to publishing a test build. Ideally this should be built into `npm publish` but isn't for now..
+Simple script to put in front of your test build server to change the package name and optionally add in a build number to the version prior to publishing a test build.
 
 ## Install
 
@@ -8,4 +8,4 @@ Simple script to put in front of your test build server to change the package na
 
 ## Usage
 
-`testbuild package_name [directory]`
+`testbuild package_name [build_number] [directory]`
--- index.js
+++ index.js
@@ -6,19 +6,22 @@ var fs = require('fs'),
 if (require.main === module) {
 	if (process.argv.length < 3) {
 		console.log('Update the package.json file to the test build package name.');
-		console.log('Usage: testbuild package_name [directory]');
+		console.log('Usage: testbuild package_name [build] [directory]');
 	} else {
 		var package_name = process.argv[2],
+			build,
 			directory = process.cwd();
 
-		if (typeof process.argv[3] !== 'undefined') directory = process.argv[3];
+		if (typeof process.argv[3] !== 'undefined') build = process.argv[3];
+		if (typeof process.argv[4] !== 'undefined') directory = process.argv[4];
 
 		var package_json = fs.readFileSync(directory + path.sep + 'package.json', 'ascii');
 
 		package_json = JSON.parse(package_json);
 		package_json.name = package_name;
+		if (build) package_json.version += '.' + build;
 
 		fs.writeFileSync(directory + path.sep + 'package.json', JSON.stringify(package_json, null, '    '));
-		console.log('Complete. Package name is "' + package_json.name + '".');
+		console.log('Complete. Package name is "' + package_json.name + '", version is "' + package_json.version + '"');
 	}
 }
